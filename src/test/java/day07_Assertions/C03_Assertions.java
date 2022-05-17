36331121238c88bb283b661fe1169bd35623a479
murat akdeniz <akdenizmurat5205@gmail.com>
2022-05-17 17:07:27 +0300

    16.05.2022

---
+++ src/test/java/day07_Assertions/C03_Assertions.java
@@ -0,0 +1,15 @@
+package day07_Assertions;
+
+public class C03_Assertions {
+
+    // Bir Class olusturalim YanlisEmailTesti
+    //http://automationpractice.com/index.php sayfasina gidelim
+    //Sign in butonuna basalim
+    //Email kutusuna @isareti olmayan bir mail yazip enter’a bastigimizda
+    //   “Invalid email address” uyarisi ciktigini test edelim
+
+
+
+
+
+}
